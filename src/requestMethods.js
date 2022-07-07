30e9640f6894042a58bc0fd415e1c590f8c17d2e
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2022-07-07 22:39:10 +0300

    Changed the enviroment variables for accessing the backend api endpoints

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:5000/api/";
+// change the base url to production url
+// "REACT_APP_API_PATH_PRODUCTION"
+const BASE_URL = process.env.REACT_APP_API_PATH;
+console.log(BASE_URL);
 let TOKEN = ""
 
 if ("persist:root" in localStorage) {
